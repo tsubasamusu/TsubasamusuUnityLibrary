a82053bfeb761db6b70bafc39282bee246b3f435
tsubasamusu <tsubasa_03_08@icloud.com>
2024-03-04 22:13:44 +0900

    [add] #1 「UnityWebRequest.SendWebRequest()」を await で待機可能にする処理を追加

---
+++ UnityWebRequestAwaiter/ProgressUpdater.cs
@@ -0,0 +1,43 @@
+﻿#pragma warning disable CS8618
+#pragma warning disable CS8625
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TSUBASAMUSU.UnityWebRequestAwaiter
+{
+    public class ProgressUpdater : MonoBehaviour
+    {
+        private static ProgressUpdater instance;
+
+        private List<WebRequestProgressNotifier> webRequestProgressNotifiers = new List<WebRequestProgressNotifier>();
+
+        public static ProgressUpdater Instance
+        {
+            get
+            {
+                if (instance == null) instance = new GameObject("ProgressUpdater").AddComponent<ProgressUpdater>();
+
+                return instance;
+            }
+        }
+
+        public void AddItem(WebRequestProgressNotifier webRequestProgressNotifier)
+        {
+            if (!webRequestProgressNotifier.NotifyProgress()) webRequestProgressNotifiers.Add(webRequestProgressNotifier);
+        }
+
+#pragma warning disable IDE0051
+        private void Update()
+#pragma warning restore IDE0051
+        {
+            for (int i = 0; i < webRequestProgressNotifiers.Count; i++)
+            {
+                WebRequestProgressNotifier webRequestProgressNotifier = webRequestProgressNotifiers[i];
+
+                if (webRequestProgressNotifier.NotifyProgress()) webRequestProgressNotifiers[i] = null;
+            }
+
+            webRequestProgressNotifiers.RemoveAll(webRequestProgressNotifier => webRequestProgressNotifier == null);
+        }
+    }
+}
