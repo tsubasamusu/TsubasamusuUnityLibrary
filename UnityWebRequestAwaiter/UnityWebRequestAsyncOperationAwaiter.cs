13637f71afb9dd6f7e59a7a0e08f06fd29156109
tsubasamusu <tsubasa_03_08@icloud.com>
2024-03-04 22:13:44 +0900

    [add] #1 「UnityWebRequest.SendWebRequest()」を await で待機可能にする処理を追加

---
+++ UnityWebRequestAwaiter/UnityWebRequestAsyncOperationAwaiter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using UnityEngine.Networking;
+
+namespace TSUBASAMUSU.UnityWebRequestAwaiter
+{
+    public class UnityWebRequestAsyncOperationAwaiter : INotifyCompletion
+    {
+        private UnityWebRequestAsyncOperation unityWebRequestAsyncOperation;
+
+        public bool IsCompleted
+        {
+            get => unityWebRequestAsyncOperation.isDone;
+        }
+
+        public UnityWebRequestAsyncOperationAwaiter(UnityWebRequestAsyncOperation unityWebRequestAsyncOperation)
+        {
+            this.unityWebRequestAsyncOperation = unityWebRequestAsyncOperation;
+        }
+
+        public void GetResult()
+        {
+
+        }
+
+        public void OnCompleted(Action action) => unityWebRequestAsyncOperation.completed += _ => { action(); };
+
+        public UnityWebRequestAsyncOperationAwaiter GetAwaiter() => this;
+    }
+}
---
+++ UnityWebRequestAwaiter/UnityWebRequestAsyncOperationExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine.Networking;
+
+namespace TSUBASAMUSU.UnityWebRequestAwaiter
+{
+    public static class UnityWebRequestAsyncOperationExtension
+    {
+        public static UnityWebRequestAsyncOperationAwaiter GetAwaiter(this UnityWebRequestAsyncOperation unityWebRequestAsyncOperation) => new UnityWebRequestAsyncOperationAwaiter(unityWebRequestAsyncOperation);
+
+        public static UnityWebRequestAsyncOperationAwaiter ConfigureAwait(this UnityWebRequestAsyncOperation unityWebRequestAsyncOperation, IProgress<float> progress)
+        {
+            WebRequestProgressNotifier webRequestProgressNotifier = new WebRequestProgressNotifier(unityWebRequestAsyncOperation, progress);
+
+            ProgressUpdater.Instance.AddItem(webRequestProgressNotifier);
+
+            return new UnityWebRequestAsyncOperationAwaiter(unityWebRequestAsyncOperation);
+        }
+    }
+}
