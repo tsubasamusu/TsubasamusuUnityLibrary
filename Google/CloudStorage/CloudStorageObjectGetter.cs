bab942778b48711b6fb52e2e837e59cf35c231f9
tsubasamusu <tsubasa_03_08@icloud.com>
2024-05-19 16:45:32 +0900

    [update] #15 GetAssetFromCloudStorage() を作成（動作確認済み）

--- Google/CloudStorage/CloudStorageObjectGetter.cs
+++ Google/CloudStorage/CloudStorageObjectGetter.cs
@@ -13,7 +13,7 @@ namespace TSUBASAMUSU.Google.CloudStorage
     public static class CloudStorageObjectGetter
     {
         /// <summary>
-        /// Get the texture in Google Cloud Storage.
+        /// Get the texture in  the Google Cloud Storage.
         /// </summary>
         public static async Task<Texture2D> GetTextureFromCloudStorageAsync(string googleCloudJwt, string bucketName, string objectName, int textureWidth = 512, int textureHeight = 512)
         {
@@ -60,9 +60,58 @@ namespace TSUBASAMUSU.Google.CloudStorage
         }
 
         /// <summary>
-        /// Get the prefab in Google Cloud Storage.
+        /// Get the asset in  the Google Cloud Storage with AssetBundle.
         /// </summary>
-        public static async Task<GameObject> GetPrefabFromCloudStorageAsync(string googleCloudJwt, string bucketName, string objectName, string prefabName)
+        public static async Task<T> GetAssetFromCloudStorage<T>(string googleCloudJwt, string bucketName, string objectName, string assetName) where T : UnityEngine.Object
+        {
+            AssetBundle assetBundle = await GetAssetBundleFromCloudStorageAsync(googleCloudJwt, bucketName, objectName);
+
+            if (assetBundle == null) return null;
+
+            return await GetAssetFromAssetBundle<T>(assetBundle, assetName);
+        }
+
+        private static async Task<T> GetAssetFromAssetBundle<T>(AssetBundle assetBundle, string assetName) where T : UnityEngine.Object
+        {
+            if (assetBundle == null)
+            {
+                Debug.LogError("AssetBundle is null.");
+
+                return null;
+            }
+
+            T asset;
+
+            try
+            {
+                AssetBundleRequest assetBundleRequest = assetBundle.LoadAssetAsync<T>(assetName);
+
+                await Task.Yield();
+
+                while (!assetBundleRequest.isDone) await Task.Delay(100);
+
+#pragma warning disable CS8600
+                asset = assetBundleRequest.asset as T;
+#pragma warning restore CS8600
+            }
+            catch (Exception exception)
+            {
+                Debug.LogException(exception);
+
+                return null;
+            }
+
+            if (asset == null)
+            {
+                Debug.LogError("Failed to load \"" + assetName + "\" from AssetBundle.");
+
+                return null;
+            }
+
+            return asset;
+        }
+
+        private static async Task<AssetBundle> GetAssetBundleFromCloudStorageAsync(string googleCloudJwt, string bucketName, string objectName)
         {
             NameValueCollection nameValueCollection = HttpUtility.ParseQueryString(string.Empty);
 
@@ -123,27 +172,7 @@ namespace TSUBASAMUSU.Google.CloudStorage
                 return null;
             }
 
-            GameObject gameObject;
-
-            try
-            {
-                gameObject = assetBundle.LoadAsset<GameObject>(prefabName);
-            }
-            catch (Exception exception)
-            {
-                Debug.LogException(exception);
-
-                return null;
-            }
-
-            if (gameObject == null)
-            {
-                Debug.LogError("Failed to convert prefab \"" + prefabName + "\" from AssetBundle.");
-
-                return null;
-            }
-
-            return gameObject;
+            return assetBundle;
         }
     }
 }
