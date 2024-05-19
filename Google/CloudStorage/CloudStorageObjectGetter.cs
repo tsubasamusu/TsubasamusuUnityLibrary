f55428886efb41d01fd2507614caf297672c6f48
tsubasamusu <tsubasa_03_08@icloud.com>
2024-05-19 13:55:46 +0900

    [update] #12 CloudStorageObjectGetter.GetPrefabFromCloudStorageAsync() が完成（動作確認済み）

--- Google/CloudStorage/CloudStorageObjectGetter.cs
+++ Google/CloudStorage/CloudStorageObjectGetter.cs
@@ -58,5 +58,92 @@ namespace TSUBASAMUSU.Google.CloudStorage
 
             return DownloadHandlerTexture.GetContent(unityWebRequest);
         }
+
+        /// <summary>
+        /// Get the prefab in Google Cloud Storage.
+        /// </summary>
+        public static async Task<GameObject> GetPrefabFromCloudStorageAsync(string googleCloudJwt, string bucketName, string objectName, string prefabName)
+        {
+            NameValueCollection nameValueCollection = HttpUtility.ParseQueryString(string.Empty);
+
+            nameValueCollection.Add("alt", "media");
+
+            string apiUrl = "https://www.googleapis.com/storage/v1/b/" + bucketName + "/o/" + objectName + "?" + nameValueCollection.ToString();
+
+            using UnityWebRequest unityWebRequest = new UnityWebRequest(apiUrl, "GET")
+            {
+                downloadHandler = new DownloadHandlerAssetBundle(apiUrl, 0)
+            };
+
+            Dictionary<string, string> headers = new Dictionary<string, string>()
+            {
+                {"Authorization", "Bearer " + googleCloudJwt},
+                {"Content-type", "application/x-www-form-urlencoded"}
+            };
+
+            foreach (KeyValuePair<string, string> header in headers) unityWebRequest.SetRequestHeader(header.Key, header.Value);
+
+            try
+            {
+                await unityWebRequest.SendWebRequest();
+            }
+            catch (Exception exception)
+            {
+                Debug.LogException(exception);
+
+                return null;
+            }
+
+            if (unityWebRequest.result == UnityWebRequest.Result.ConnectionError || unityWebRequest.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.LogError(unityWebRequest.result.ToString());
+
+                Debug.LogError(unityWebRequest.downloadHandler.text);
+
+                return null;
+            }
+
+            AssetBundle assetBundle;
+
+            try
+            {
+                assetBundle = DownloadHandlerAssetBundle.GetContent(unityWebRequest);
+            }
+            catch (Exception exception)
+            {
+                Debug.LogException(exception);
+
+                return null;
+            }
+
+            if (assetBundle == null)
+            {
+                Debug.LogError("Failed to convert AssetBundle from content of sent unity web request.");
+
+                return null;
+            }
+
+            GameObject gameObject;
+
+            try
+            {
+                gameObject = assetBundle.LoadAsset<GameObject>(prefabName);
+            }
+            catch (Exception exception)
+            {
+                Debug.LogException(exception);
+
+                return null;
+            }
+
+            if (gameObject == null)
+            {
+                Debug.LogError("Failed to convert prefab \"" + prefabName + "\" from AssetBundle.");
+
+                return null;
+            }
+
+            return gameObject;
+        }
     }
 }
