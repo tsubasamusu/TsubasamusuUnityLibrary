776e8bf8caf406ef921fb612b8be17ff5e91b507
tsubasamusu <tsubasa_03_08@icloud.com>
2024-05-22 20:16:09 +0900

    [refactor] #20 LightingUtility.cs を Other フォルダに移動して ApplyLightmapsToCurrentScene() を作成

---
+++ Other/LightingUtility.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace TsubasamusuUnityLibrary.Other
+{
+    public static class LightingUtility
+    {
+        public static void ApplyLightmapsToCurrentScene(Texture2D[] colorLightmaps, Texture2D[] dirLightmaps)
+        {
+            LightmapData[] lightmapDatas = new LightmapData[colorLightmaps.Length];
+
+            for (int i = 0; i < colorLightmaps.Length; i++)
+            {
+                LightmapData lightmapData = new LightmapData();
+
+                lightmapData.lightmapColor = colorLightmaps[i];
+
+                lightmapData.lightmapDir = dirLightmaps[i];
+
+                lightmapDatas[i] = lightmapData;
+            }
+
+            LightmapSettings.lightmaps = lightmapDatas;
+        }
+    }
+}
