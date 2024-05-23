979f562f5aaa89c9c277d8791d5e548d5f6bcedc
tsubasamusu <tsubasa_03_08@icloud.com>
2024-05-23 10:54:52 +0900

    [add] #20 LightingUtility.cs を Other フォルダから Lighting フォルダに移動して ApplyLightmapToMeshRenderer() を追加し、同じディレクトリに MeshRendererLightmapData.cs を作成（動作未確認）

---
+++ Lighting/LightingUtility.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+namespace TSUBASAMUSU.Lighting
+{
+    public static class LightingUtility
+    {
+        public static void ApplyLightmapsToCurrentScene(Texture2D[] colorLightmaps, Texture2D[] dirLightmaps)
+        {
+            LightmapData[] lightmapDatas = new LightmapData[colorLightmaps.Length];
+
+            for (int i = 0; i < colorLightmaps.Length; i++)
+            {
+                LightmapData lightmapData = new LightmapData();
+
+                lightmapData.lightmapColor = colorLightmaps[i];
+
+                lightmapData.lightmapDir = dirLightmaps[i];
+
+                lightmapDatas[i] = lightmapData;
+            }
+
+            LightmapSettings.lightmaps = lightmapDatas;
+        }
+
+        public static void ApplyLightmapToMeshRenderer(MeshRendererLightmapData meshRendererLightmapData)
+        {
+            GameObject meshRendererGameObject = GameObject.Find(meshRendererLightmapData.gameObjectName);
+
+            if (meshRendererGameObject == null)
+            {
+                Debug.LogError("Failed to get GameObject named \"" + meshRendererLightmapData.gameObjectName + "\".");
+
+                return;
+            }
+
+            if (!meshRendererGameObject.TryGetComponent(out MeshRenderer meshRenderer))
+            {
+                Debug.LogError("Failed to get MeshRenderer from \"" + meshRendererLightmapData.gameObjectName + "\".");
+
+                return;
+            }
+
+            meshRenderer.lightmapIndex = meshRendererLightmapData.lightmapIndex;
+
+            meshRenderer.lightmapScaleOffset = meshRendererLightmapData.lightmapScaleOffset;
+        }
+    }
+}
---
+++ Lighting/MeshRendererLightmapData.cs
@@ -0,0 +1,16 @@
+﻿#pragma warning disable CS8618
+using System;
+using UnityEngine;
+
+namespace TSUBASAMUSU.Lighting
+{
+    [Serializable]
+    public class MeshRendererLightmapData
+    {
+        public string gameObjectName;
+
+        public int lightmapIndex;
+
+        public Vector4 lightmapScaleOffset;
+    }
+}
